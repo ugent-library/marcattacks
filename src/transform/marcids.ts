bfa5f211155798d34aa9dc15f7f7f975974fb53b
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2026-01-14 15:36:36 +0100

    Adding marcids mapper

---
+++ src/transform/marcids.ts
@@ -0,0 +1,19 @@
+import { Transform } from "stream";
+import { marcmap } from "../marcmap.js";
+
+export async function transform(_opts: any) : Promise<Transform> {
+    return new Transform({
+        objectMode: true,
+        transform(data: any, _encoding, callback) {
+            let rec : string[][] = data['record'];
+
+            if (!rec) return;
+
+            let id = marcmap(rec,"001",{});
+
+            if (id.length == 1) {
+                callback(null,{id: id[0]});
+            }
+        }
+    });
+}
