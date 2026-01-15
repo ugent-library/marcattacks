6e1e3d3d3db1b19f251c3df7a8f7b594d54baa92
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2026-01-15 08:09:21 +0100

    Adding a TSV outputter

---
+++ src/output/tsv.ts
@@ -0,0 +1,38 @@
+import { Transform } from 'stream';
+import log4js from 'log4js';
+
+const logger = log4js.getLogger();
+
+export async function transform() : Promise<Transform> {
+    let sortedKeys : string[];
+
+    return new Transform({
+        objectMode: true,
+        transform(data: any, _encoding, callback) {
+            let output = "";
+            let fields : string[] = [];
+
+            if (! sortedKeys ) {
+                sortedKeys = Object.keys(data).sort();
+                output += sortedKeys.join("\t") + "\n";
+            }
+
+            sortedKeys.forEach( key => {
+                if (Array.isArray(data[key])) {
+                    fields.push(`ARRAY[${data[key].length}]`);
+                }
+                else if (typeof data[key] === 'object') {
+                    fields.push(`HASH[${Object.keys(data[key]).length}]`);
+                }
+                else {
+                    fields.push(data[key]);
+                }
+            });
+
+            output += fields.join("\t") + "\n";
+
+            logger.trace(`adding ${output.length} bytes`);
+            callback(null,output);
+        }
+    });
+}
