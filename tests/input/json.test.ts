16c1bdc1fcde3314f52a35b936d6f10dcf1c73ab
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2026-02-15 13:45:43 +0100

    Adding tests

--- tests/input/json.test.ts
+++ tests/input/json.test.ts
@@ -3,19 +3,13 @@ import { loadPlugin } from "../../dist/plugin-loader.js";
 import { Readable } from 'node:stream';
 
 const data = `
-<?xml version="1.0" encoding="UTF-8"?>
-<marc:collection xmlns:marc="http://www.loc.gov/MARC21/slim">
-  <marc:record>
-    <marc:controlfield tag="001">990036760400409161</marc:controlfield>
-    <marc:datafield tag="035" ind1=" " ind2=" ">
-      <marc:subfield code="a">(RUG01)003676040</marc:subfield>
-    </marc:datafield>
-    <marc:datafield tag="100" ind1="1" ind2=" ">
-      <marc:subfield code="a">Cassiers, Paul,</marc:subfield>
-      <marc:subfield code="d">1965-2025</marc:subfield>
-    </marc:datafield>
-  </marc:record>
-</marc:collection>
+[{
+    "record": [
+        ["001", " ", " ", "_", "990036760400409161"],
+        ["035", " ", " ", "a", "(RUG01)003676040"],
+        ["100", "1", " ", "a", "Cassiers, Paul,", "d", "1965-2025"]
+    ]
+}]
 `.trim();
 
 const json = {
@@ -26,9 +20,9 @@ const json = {
     ]
 };
 
-describe("input/xml", () => {
+describe("input/json", () => {
     test("transform converts input correctly", async () => {
-        const plugin = await loadPlugin("xml", "input");
+        const plugin = await loadPlugin("json", "input");
         const transformer = await plugin.transform(); 
 
         const results: any[] = [];
