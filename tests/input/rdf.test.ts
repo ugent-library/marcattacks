49438fabf14b34bd05f057e07292308cda6ec16d
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2026-02-18 09:09:50 +0100

    Adding rdf test

--- tests/input/rdf.test.ts
+++ tests/input/rdf.test.ts
@@ -1,14 +1,18 @@
 import { describe, test, expect } from "@jest/globals";
 import { loadPlugin } from "../../dist/plugin-loader.js";
 import { Readable } from 'node:stream';
+import type { Quad } from '../../dist/types/quad.js'
 
 const data = `
 @prefix : <http://example.org/>.
 
 :a :b :c.
+:a :b "foo"^^:bar.
+:a :b "foo"@id.
 `.trim();
 
-const json = {
+const expected = [
+{
   "quads": [{
     "subject": {
         "type": "NamedNode",
@@ -23,22 +27,58 @@ const json = {
         "value": "http://example.org/c"
     }
   }]
-};
+},
+{
+  "quads": [{
+    "subject": {
+        "type": "NamedNode",
+        "value": "http://example.org/a"
+    },
+    "predicate": {
+        "type": "NamedNode",
+        "value": "http://example.org/b"
+    },
+    "object": {
+        "type": "Literal",
+        "value": "foo",
+        "as": "http://example.org/bar"
+    }
+  }]
+},
+{
+  "quads": [{
+    "subject": {
+        "type": "NamedNode",
+        "value": "http://example.org/a"
+    },
+    "predicate": {
+        "type": "NamedNode",
+        "value": "http://example.org/b"
+    },
+    "object": {
+        "type": "Literal",
+        "value": "foo",
+        "as": "http://www.w3.org/1999/02/22-rdf-syntax-ns#langString",
+        "language": "id"
+    }
+  }]
+}
+];
 
 describe("input/rdf", () => {
     test("transform converts input correctly", async () => {
         const plugin = await loadPlugin("rdf", "input");
         const transformer = await plugin.transform(); 
 
-        const results: any[] = [];
+        const results: Quad[] = [];
     
         await new Promise((resolve, reject) => {
-            transformer.on('data', (chunk: any) => results.push(chunk));
+            transformer.on('data', (chunk: Quad) => results.push(chunk));
             transformer.on('end', resolve);
             transformer.on('error', reject);
             Readable.from(data + "\n").pipe(transformer);
         });
 
-        expect(results[0]).toStrictEqual(json);
+        expect(results).toStrictEqual(expected);
     });
 });
