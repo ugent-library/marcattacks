a0b86896df92bd95100ceff7ca350b3980eacdb8
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2026-05-21 15:16:41 +0200

    Adding config

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+import { createDefaultEsmPreset } from "ts-jest";
+
+const presetConfig = createDefaultEsmPreset({});
+
+export default {
+  ...presetConfig,
+  testEnvironment: 'node',
+  testMatch: [
+    '**/tests/**/*.test.ts',
+    '**/__tests__/**/*.test.ts',
+    '**/?(*.)+(spec|test).ts'
+  ],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  testTimeout: 30000,
+  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts'],
+};
